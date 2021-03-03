f0b77a4b84bc4aa11b0c4aca2dfe4a8050e9ed28
JitenMehta01 <40121263+JitenMehta01@users.noreply.github.com>
2021-03-03 14:33:25 +0000

    Added phrases and random phrases methods in Game class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,35 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+    constructor(){
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = [];
+    }
+
+    /**
+     * Phrases to be used in the game
+     * @return {array} returns an array of phrases.
+     */
+
+    createPhrases(){
+    const phrases = [{phrase:'The Godfather'}, 
+    {phrase:'Shawshank Redemption'},
+    {phrase: 'The Lion King'}, 
+    {phrase:'Forrest Gump'}, 
+    {phrase:'The Silence Of Lambs'}];
+
+    return phrases;
+    }
+
+    /**
+     * returns a random phrase
+     * @return {object} An object containing a phrase.
+     */
+
+    getRandomPhrase(){
+    const phraseLength = this.phrases.length;
+    let randomNum = Math.floor(Math.random() * phraseLength);
+    
+    return this.phrases[randomNum];
+        
+    }
+}
